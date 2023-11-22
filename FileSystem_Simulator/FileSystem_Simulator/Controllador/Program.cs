f1ca93409fa0b6d0d29663bbf8936e59c489497a
ParraJesus <jgparra@unicauca.edu.co>
2023-11-22 12:53:31 -0500

    clase command

--- FileSystem_Simulator/FileSystem_Simulator/Controllador/Program.cs
+++ FileSystem_Simulator/FileSystem_Simulator/Controllador/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FileSystem_Simulator.Modelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
---
+++ FileSystem_Simulator/FileSystem_Simulator/Modelo/Command.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSystem_Simulator.Modelo
+{
+    internal class Command
+    {
+        public void executeCommand(string command)
+        {
+            string[] parts = command.Split(' ');
+
+            string mainPart = parts[0].ToLower();
+
+            switch (mainPart)
+            {
+                case "hola":
+                    executeHola(parts);
+                    break;
+                case "adios":
+
+                    break;
+                default:
+
+                    break;
+            }
+
+        }
+
+        private void executeHola(string[] parts) 
+        {
+            MessageBox.Show(string.Join(" ", parts), "Imprimiendo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //return string.Join(" ", parts);
+        }
+
+    }
+}
