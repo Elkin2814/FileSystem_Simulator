37e2bfae136b3f7fc173bf6b34569c6a59c73063
ParraJesus <jgparra@unicauca.edu.co>
2023-11-22 16:36:14 -0500

    comando echo y history

--- FileSystem_Simulator/FileSystem_Simulator/Vista/Terminal.cs
+++ FileSystem_Simulator/FileSystem_Simulator/Vista/Terminal.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using FileSystem_Simulator.Modelo;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,8 @@ namespace FileSystem_Simulator
 {
     public partial class Terminal : Form
     {
+        Command command = new Command(); 
+
         private string prompt = "usuario@ubuntu:-$ ";
         public Terminal()
         {
@@ -34,8 +38,11 @@ namespace FileSystem_Simulator
 
             if (e.KeyCode == Keys.Enter)
             {
+                execute(promptLength);   
+
                 rtbTerminal.AppendText(Environment.NewLine + prompt);
-                e.SuppressKeyPress = true; 
+                e.SuppressKeyPress = true;
+
                 return;
             }
             if (e.KeyCode == Keys.Back)
@@ -62,6 +69,17 @@ namespace FileSystem_Simulator
         {
             rtbTerminal.SelectionStart = rtbTerminal.Text.Length;
             rtbTerminal.SelectionLength = 0;
+        }
+
+        private void execute(int promptLength) 
+        {
+            string commandText = rtbTerminal.Lines.Last().Substring(promptLength);
+            Debug.WriteLine("terminal: " + rtbTerminal.Lines.Last().Substring(promptLength));
+            
+            string result = command.executeCommand(commandText);
+
+            rtbTerminal.AppendText("\n" + result);
+
         }
     }
 }
